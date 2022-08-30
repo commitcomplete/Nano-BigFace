74a027ac27a6795279b6cc5a6ebe493e7a5ffd23
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-08-30 20:14:58 +0900

    [Feat] 기본 델리게이트 추가 , 사진 관련 함수 추가

--- BigFace/BigFace/ViewController.swift
+++ BigFace/BigFace/ViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import ARKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,5 +19,49 @@ class ViewController: UIViewController {
     }
 
 
+    // 사진 찍을때 깜빡임 효과
+    func showScreenshotEffect() {
+        let snapshotView = UIView()
+        snapshotView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(snapshotView)
+        // Activate full screen constraints
+        let constraints:[NSLayoutConstraint] = [
+            snapshotView.topAnchor.constraint(equalTo: view.topAnchor),
+            snapshotView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            snapshotView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            snapshotView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+        // White because it's the brightest color
+        snapshotView.backgroundColor = UIColor.black
+        // Animate the alpha to 0 to simulate flash
+        UIView.animate(withDuration: 0.2, animations: {
+            snapshotView.alpha = 0
+        }) { _ in
+            // Once animation completed, remove it from view.
+            snapshotView.removeFromSuperview()
+        }
+    }
+    // ui image 저장
+    func saveInPhoto(img: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(img, nil, nil, nil)
+    }
+
+    // ui image 공유
+    func sharePicture(img: UIImage) {
+        let av = UIActivityViewController(activityItems: [img], applicationActivities: nil)
+        UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+    }
+}
+
+extension ViewController : ARSCNViewDelegate{
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        <#code#>
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        <#code#>
+    }
+    
 }
 
