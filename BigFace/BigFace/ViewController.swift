21dff1b1b8e2eb5f5c0d14714a13cd24291e509c
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-08-30 20:45:34 +0900

    [Feat] BigFace 생성 완료

--- BigFace/BigFace/ViewController.swift
+++ BigFace/BigFace/ViewController.swift
@@ -12,10 +12,19 @@ import CoreImage.CIFilterBuiltins
 import AVFoundation
 
 class ViewController: UIViewController {
+    private let sceneView = ARSCNView(frame: UIScreen.main.bounds)
+    //The CoreML model we use for emotion classification.
+    private let model = try! VNCoreMLModel(for: CNNEmotions().model)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard ARWorldTrackingConfiguration.isSupported else { return }
+        view.addSubview(sceneView)
+        sceneView.delegate = self
+        let ARFace: ARFaceTrackingConfiguration = ARFaceTrackingConfiguration();
+        ARFace.maximumNumberOfTrackedFaces = 5
+        sceneView.session.run(ARFace, options: [.resetTracking, .removeExistingAnchors])
     }
     
     // MARK: - make Color Filters
@@ -153,13 +162,54 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: BigFace 생성
 extension ViewController : ARSCNViewDelegate{
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        <#code#>
+        guard let device = sceneView.device else { return nil }
+        guard let sceneView = renderer as? ARSCNView,
+              let frame = sceneView.session.currentFrame,
+              anchor is ARFaceAnchor
+        else { return nil }
+        
+        let faceGeometry = ARSCNFaceGeometry(device: sceneView.device!, fillMesh: true)!
+        let material = faceGeometry.firstMaterial!
+        material.diffuse.contents = sceneView.scene.background.contents
+        material.lightingModel = .constant
+        
+        guard let shaderURL = Bundle.main.url(forResource: "VideoTexturedFace", withExtension: "shader"),
+              let modifier = try? String(contentsOf: shaderURL)
+        else { fatalError("Can't load shader modifier from bundle.") }
+        faceGeometry.shaderModifiers = [ .geometry: modifier]
+        // Pass view-appropriate image transform to the shader modifier so
+        // that the mapped video lines up correctly with the background video.
+        let affineTransform = (frame.displayTransform(for: .portrait, viewportSize: sceneView.bounds.size))
+        let transform = SCNMatrix4(affineTransform)
+        faceGeometry.setValue(SCNMatrix4Invert(transform), forKey: "displayTransform")
+        let node = SCNNode(geometry: faceGeometry)
+        
+        return node
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        <#code#>
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+              let faceGeometry = node.geometry as? ARSCNFaceGeometry,
+              let pixelBuffer = self.sceneView.session.currentFrame?.capturedImage,
+              let sceneView = renderer as? ARSCNView
+        else {
+            return
+        }
+        //Updates the face geometry.
+        faceGeometry.update(from: faceAnchor.geometry)
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .right, options: [:]).perform([VNCoreMLRequest(model: model) { [weak self] request, error in
+            //Here we get the first result of the Classification Observation result.
+            guard let firstResult = (request.results as? [VNClassificationObservation])?.first else { return }
+            DispatchQueue.main.async { [self] in
+                if firstResult.confidence > 0.92 {
+                    print(123432)
+                }
+            }
+    }])
     }
     
 }
